3348f6b3d27f1bb1934ecad97d5faa352963442a
abdullarahimli <abdullavr@code.edu.az>
2022-07-05 11:46:59 +0400

    First commit

---
+++ P225Allup/P225Allup/Controllers/AboutController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace P225Allup.Controllers
+{
+    public class AboutController : Controller
+    {
+        [Authorize(Roles = "SuperAdmin,Member")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
